2da4b89cd2fa6df68a2e381675ef3c7e543dda76
Yizheng Yu <yalex778@163.com>
2024-01-06 22:24:12 -0500

    Added add point and segment methods

--- js/math/graph.js
+++ js/math/graph.js
@@ -3,7 +3,39 @@ class Graph {
         this.points = points;
         this.segments = segments;
     }
+
+    addPoint(point) {
+        this.points.push(point);
+    }
+
+    containsPoint(point) {
+        return this.points.find((p) => p.equals(point)); 
+    }
+
+    tryAddPoint(point) {
+        if (!this.containsPoint(point)) {
+            this.addPoint(point);
+            return true;
+        }
+        return false;
+    }
+
+    addSegment(seg) {
+        this.segments.push(seg);
+    }
+
+    containsSegment(seg) {
+        return this.segments.find((s) => s.equals(seg));
+    }
     
+    tryAddSegment(seg) {
+        if (!this.containsSegment(seg) && !seg.p1.equals(seg.p2)) {
+            this.addSegment(seg);
+            return true;
+        }
+        return false;
+    }
+
     draw(ctx) {
         for (const seg of this.segments) {
             seg.draw(ctx);
