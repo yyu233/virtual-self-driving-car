00208fc2de4c01053fed5ccb51c4c3e28d48c2ea
Yizheng Yu <yalex778@163.com>
2024-01-08 03:24:43 -0500

    Added graphEditor and selec point methods

---
+++ js/graphEditor.js
@@ -0,0 +1,34 @@
+class GraphEditor {
+    constructor(canvas, graph) {
+        this.canvas = canvas;
+        this.graph = graph;
+
+        this.ctx = this.canvas.getContext("2d");
+        
+        this.selected = null;
+        this.hovered = null;
+
+        this.#addEventListeners();
+    }
+
+    #addEventListeners(){
+        this.canvas.addEventListener("mousedown", (evt) => {
+            const mouse = new Point(evt.offsetX, evt.offsetY);
+            this.hovered = getNearestPoint(mouse, this.graph.points);
+            if (this.hovered) {
+                this.selected = this.hovered;
+                return;
+            }
+            this.graph.addPoint(mouse);
+            this.selected = mouse;
+        });
+    }
+
+    display() {
+        this.graph.draw(this.ctx);
+
+        if (this.selected) {
+            this.selected.draw(this.ctx, { outline: true });
+        }
+    }
+}
